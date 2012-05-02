d04ae1bcd7fb9010f8727c10204ec2170d9f35c4
t0mpr1c3 <t0mpr1c3@gmail.com>
2012-05-02 20:25:19 +0200

    Add dithering to smooth quantization errors

--- PID_v1/PID_v1.h
+++ PID_v1/PID_v1.h
@@ -4,43 +4,45 @@
 
 class PID
 {
-
-
-  public:
-
-  //Constants used in some of the functions below
-  #define AUTOMATIC	1
-  #define MANUAL	0
-  #define DIRECT  0
-  #define REVERSE  1
-
-  //commonly used functions **************************************************************************
-    PID(double*, double*, double*,        // * constructor.  links the PID to the Input, Output, and 
-        double, double, double, int);     //   Setpoint.  Initial tuning parameters are also set here
 	
-    void SetMode(int Mode);               // * sets PID to either Manual (0) or Auto (non-0)
-
-    void Compute();                       // * performs the PID calculation.  it should be
-                                          //   called every time loop() cycles. ON/OFF and
-                                          //   calculation frequency can be set using SetMode
-                                          //   SetSampleTime respectively
-
-    void SetOutputLimits(double, double); //clamps the output to a specific range. 0-255 by default, but
+	
+	public:
+	
+	//Constants used in some of the functions below
+	#define AUTOMATIC	1
+	#define MANUAL	0
+	#define DIRECT  0
+	#define REVERSE  1
+	
+  //commonly used functions **************************************************************************
+	PID(double*, double*, double*,        // * constructor.  links the PID to the Input, Output, and 
+	double, double, double, int);     //   Setpoint.  Initial tuning parameters are also set here
+	
+	void SetMode(int Mode);               // * sets PID to either Manual (0) or Auto (non-0)
+	
+	void Compute();                       // * performs the PID calculation.  it should be
+	                                  //   called every time loop() cycles. ON/OFF and
+	                                  //   calculation frequency can be set using SetMode
+	                                  //   SetSampleTime respectively
+	
+	void SetOutputLimits(double, double); //clamps the output to a specific range. 0-255 by default, but
 										  //it's likely the user will want to change this depending on
 										  //the application
 	
 
 
   //available but not commonly used functions ********************************************************
-    void SetTunings(double, double,       // * While most users will set the tunings once in the 
-                    double);         	  //   constructor, this function gives the user the option
-                                          //   of changing tunings during runtime for Adaptive control
+	void SetTunings(double, double,       // * While most users will set the tunings once in the 
+	            double);         	  //   constructor, this function gives the user the option
+	                                  //   of changing tunings during runtime for Adaptive control
 	void SetControllerDirection(int);	  // * Sets the Direction, or "Action" of the controller. DIRECT
 										  //   means the output will increase when error is positive. REVERSE
 										  //   means the opposite.  it's very unlikely that this will be needed
 										  //   once it is set in the constructor.
-    void SetSampleTime(int);              // * sets the frequency, in Milliseconds, with which 
-                                          //   the PID calculation is performed.  default is 100
+	void SetSampleTime(int);              // * sets the frequency, in Milliseconds, with which 
+	                                  //   the PID calculation is performed.  default is 100
+	void SetDither(double);               // * sets the range of the dither 										  
+                                          
 										  
 										  
 										  
@@ -50,6 +52,7 @@ class PID
 	double GetKd();						  // where it's important to know what is actually 
 	int GetMode();						  //  inside the PID.
 	int GetDirection();					  //
+	double GetDither();                   // 
 
   private:
 	void Initialize();
@@ -59,15 +62,15 @@ class PID
 	double dispKd;				//
     
 	double kp;                  // * (P)roportional Tuning Parameter
-    double ki;                  // * (I)ntegral Tuning Parameter
-    double kd;                  // * (D)erivative Tuning Parameter
+	double ki;                  // * (I)ntegral Tuning Parameter
+	double kd;                  // * (D)erivative Tuning Parameter
 
 	int controllerDirection;
 
-    double *myInput;              // * Pointers to the Input, Output, and Setpoint variables
-    double *myOutput;             //   This creates a hard link between the variables and the 
-    double *mySetpoint;           //   PID, freeing the user from having to constantly tell us
-                                  //   what these values are.  with pointers we'll just know.
+	double *myInput;              // * Pointers to the Input, Output, and Setpoint variables
+	double *myOutput;             //   This creates a hard link between the variables and the 
+	double *mySetpoint;           //   PID, freeing the user from having to constantly tell us
+	                          //   what these values are.  with pointers we'll just know.
 			  
 	unsigned long lastTime;
 	double ITerm, lastInput;
@@ -75,6 +78,8 @@ class PID
 	int SampleTime;
 	double outMin, outMax;
 	bool inAuto;
+	
+	double dither, d;			// * noise added to input to smooth quantization errors, set to smallest step value in input range	
 };
 #endif
 
